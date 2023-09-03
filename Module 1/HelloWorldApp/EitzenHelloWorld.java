b34b8bcb4cf89e0ff9502258a72f6e9be8b4c554
Nolan Eitzen <neitzen@my365.bellevue.edu>
2023-09-02 19:57:48 -0500

    Added documentation requirements

--- Module 1/HelloWorldApp/EitzenHelloWorld.java
+++ Module 1/HelloWorldApp/EitzenHelloWorld.java
@@ -1,7 +1,15 @@
+/*
+ * Eitzen, N. (2023). CIS 505 Intermediate Java Programming. Bellevue University
+ */
+
 class EitzenHelloWorld{
+    /*
+    *  Method Written to test and execute assignment parameters.
+    *  @return prints a string to the console windows
+    */
     public static void main(String[] args){
         String firstname = new String("Nolan");
         String lastname = new String("Eitzen");
         System.out.printf("Hello World From " + firstname + " " + lastname + ".");
-    }
-};
+    } //end main
+} // end EitzenHelloWorld
