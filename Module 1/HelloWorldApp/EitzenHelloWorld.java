5e4ef1c19c46f6021d491dbac28905c43e10371b
Nolan Eitzen <neitzen@my365.bellevue.edu>
2023-09-02 19:49:57 -0500

    1.2 Assignment Completed

---
+++ Module 1/HelloWorldApp/EitzenHelloWorld.java
@@ -0,0 +1,7 @@
+class EitzenHelloWorld{
+    public static void main(String[] args){
+        String firstname = new String("Nolan");
+        String lastname = new String("Eitzen");
+        System.out.printf("Hello World From " + firstname + " " + lastname + ".");
+    }
+};
