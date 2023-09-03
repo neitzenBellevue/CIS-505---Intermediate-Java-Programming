962cdbfb497676a78a0e395d9041206035a0f068
Nolan Eitzen <neitzen@my365.bellevue.edu>
2023-09-02 23:12:15 -0500

    Added Comments to TestFanApp

--- Module 1/FanApp/TestFanApp.java
+++ Module 1/FanApp/TestFanApp.java
@@ -1,4 +1,8 @@
+/*
+ * Eitzen, N. (2023). CIS 505 Intermediate Java Programming. Bellevue University
+ */
 public class TestFanApp {
+    //This is for testing the Fan.Java class
     public static void main(String[] args) {
         Fan defaultFan = new Fan();
         Fan argumentFan = new Fan(2, true, 3 , "Red");
