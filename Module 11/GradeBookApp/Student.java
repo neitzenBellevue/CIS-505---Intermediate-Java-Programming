0ff87ce89e5936203c9875b2b171880d9f0590a9
Nolan Eitzen <neitzen@my365.bellevue.edu>
2023-11-12 15:55:23 -0600

    working on writing file

--- Module 11/GradeBookApp/Student.java
+++ Module 11/GradeBookApp/Student.java
@@ -4,39 +4,16 @@
 
 package GradeBookApp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Student {
+    
     private String firstName;
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     private String lastName;
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     private String courseName;
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
     private String grade;
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
 
     /*
      * This constructor takes 4 strings and assigns them to the class variables.
@@ -62,11 +39,73 @@ public class Student {
         return csv.toString();
     } // End toString
 
-    public static void writeStudents(Student[] students, String FileName){
-
-    }
+    public static void writeStudents(Student[] students, String fileName){
+        StringBuilder csv = new StringBuilder();
+        for(Student student : students){
+            csv.append(student.toString());
+        }
+        File csvOut = new File(fileName);
+        try (PrintWriter pw = new PrintWriter(csvOut) ){
+            pw.write(csv.toString());
+        } catch (FileNotFoundException e) {
+            // TODO: handle exception
+        }
+    } // End writeStudents(Student[], String)
 
     public static void readStudents(String fileName){
 
-    }
-}
+    } // End readStudent(String)
+
+    // This getter method retrieves the firstName variable.
+    public String getFirstName() {
+        return firstName;
+    }// End getFirstName()
+
+    /*
+     * This setter method changes the firstName variable.
+     * @param firstName String
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    } // End setFirstName
+
+    // This getter method retrieves the grade variable.
+    public String getGrade() {
+        return grade;
+    } // End getGrade()
+
+    /*
+     * This setter method changes the grade variable.
+     * @param grade String
+     */
+    public void setGrade(String grade) {
+        this.grade = grade;
+    } // End setGrate(String)
+
+    // This getter method retrieves the lastName variable.
+    public String getLastName() {
+        return lastName;
+    } // End getLastName()
+
+    /*
+     * This setter method changes the lastName variable.
+     * @param lastName String
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    } // End setLastName(String)
+
+    // This getter method retrieves the courseName variable.
+     public String getCourseName() {
+        return courseName;
+    } // End getCourseName()
+
+    /*
+     * This setter method changes the courseName variable.
+     * @param courseName String
+     */
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    } // End setCourseName(String)
+
+} // End Student
